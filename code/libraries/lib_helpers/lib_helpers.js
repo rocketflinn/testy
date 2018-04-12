b6656fdbbf8e95f382ae39885c4bf43cde46359c
rocketflinn <30417713+rocketflinn@users.noreply.github.com>
2018-04-12 13:48:45 -0400

    Asset created by rocketflinn

---
+++ code/libraries/lib_helpers/lib_helpers.js
@@ -0,0 +1,4 @@
+// Global Constants (Assuming ANY POINT URI will be adjusted for devices)
+const ANYPOINTURI="https://yardarm-bacnet-prod-w2.cloudhub.io/api/bacnet/devices/12312";
+const ANYPOINTUID="84ac1a9b597243b59d29e0f82d0a7806";
+const ANYPOINTPASSWORD="b8d862A81Bf84FfBaE6bb7677558c827";
